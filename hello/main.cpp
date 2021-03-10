a83d9376a760b1426fadeae69d24e3338f51cddb
soeun <70000city@gmail.com>
2021-03-10 21:32:29 +0900

    초기 커밋.

---
+++ hello/main.cpp
@@ -0,0 +1,13 @@
+#include <opencv2/opencv.hpp>
+/*
+using namespace cv;
+int main() {
+	Mat image;
+	image = imread("D:/opencv451/sources/samples/data/HappyFish.jpg");
+	imshow("Test", image);
+	waitKey();
+}
+*/
+
+
+// ctrl+f5 ½ÇÇà
