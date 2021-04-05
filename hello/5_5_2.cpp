7cb85e8e8f2ab68f60f8988d255bc435046048e6
So-EunPark <70000city@gmail.com>
2021-04-05 15:54:48 +0900

    과제3 및 4/5 수업

---
+++ hello/5_5_2.cpp
@@ -0,0 +1,23 @@
+//5ÁÖÂ÷-2 254
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+//int main() {
+//	Mat_<uchar> m1(3, 5);
+//	m1 << 11, 2, 3, 4, 10,
+//		6, 10, 15, 9, 7,
+//		7, 12, 8, 14, 1;
+//
+//	Mat m_sort1, m_sort2, m_sort3,m_sort_idx1, m_sort_idx2;
+//	cv::sort(m1, m_sort1, SORT_EVERY_ROW);
+//	cv::sort(m1, m_sort2, SORT_EVERY_ROW + SORT_DESCENDING);
+//	cv::sort(m1, m_sort3, SORT_EVERY_COLUMN);
+//
+//	cout << "[m1]=" << endl << m1 << endl << endl;
+//	cout << "[m_sort1]=" << endl << m_sort1 << endl << endl;
+//	cout << "[m_sort2]=" << endl << m_sort2 << endl << endl;
+//	cout << "[m_sort3]=" << endl << m_sort3 << endl << endl;
+//	return 0;
+//}
---
+++ hello/assmt_3.cpp
@@ -0,0 +1,64 @@
+//과제 3
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+//int main() {
+//	Mat logo = imread("D:\\4-1학기\\영상처리_한창호\\images/도깨비1.png", IMREAD_COLOR);
+//	Mat sea = imread("D:\\4-1학기\\영상처리_한창호\\images/바다.jpg", IMREAD_COLOR);
+//	Mat character = imread("D:\\4-1학기\\영상처리_한창호\\images/배우.png", IMREAD_COLOR);
+//	Mat logo_th, character_th, masks[5], masks2[5], background, background2, foreground, foreground2, dst, dst2;
+//	CV_Assert(logo.data && sea.data && character.data);
+//
+//	//로고 이진화
+//	threshold(logo, logo_th, 50, 255, THRESH_BINARY);
+//	split(logo_th, masks);
+//	
+//	//로고 포인트
+//	Point p1 = sea.size() / 10;
+//	Point p2 = logo.size() / 10;
+//	Point logo_p = p1 - p2;
+//	Rect roi(logo_p, logo.size());
+//
+//	//로고 반전
+//	bitwise_not(masks[0], masks[3]);
+//
+//	bitwise_and(logo, logo, foreground, masks[3]);
+//	bitwise_and(sea(roi), sea(roi), background, masks[3]);
+//
+//	add(background, foreground, dst);
+//	dst.copyTo(sea(roi));
+//
+//	//캐릭터
+//	threshold(character, character_th, 200, 255, THRESH_BINARY);
+//	split(character_th, masks2);
+//
+//	bitwise_or(masks2[0], masks2[1], masks2[3]);
+//	bitwise_or(masks2[2], masks2[3], masks2[3]);
+//	bitwise_not(masks2[0], masks2[3]);
+//
+//	//캐릭터 포인트
+//	Point p3 = sea.size();
+//	Point p4 = character.size();
+//	Point char_p = p3 - p4;
+//	Rect roi2(char_p, character.size());
+//
+//	//캐릭터
+//	bitwise_and(character, character, foreground2, masks2[3]);
+//	bitwise_and(sea(roi2), sea(roi2), background2, masks2[4]);
+//
+//	add(background2, foreground2, dst2);
+//	dst2.copyTo(sea(roi2));
+//
+//
+//	imshow("dst2", dst2);
+//	imshow("foreground", foreground);
+//	imshow("foreground2", foreground2);
+//	//imshow("character", character);
+//	//imshow("masks2[0])", masks2[0]);
+//	imshow("masks2[3]", masks2[3]);
+//	imshow("masks3", sea);
+//	waitKey();
+//	return 0;
+//}
